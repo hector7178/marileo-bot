2644be41a684a9a056a49bdd217fbdb62762e5af
hector7178 <hjgrimanmarcano@gmail.com>
2023-12-20 13:40:38 -0400

    cliente

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: 'npm start'}],
+
+  deploy : {
+    production : {
+      key: "marileo.pem",
+      user : 'ubuntu',
+      host : '18.231.157.113',
+      ref  : 'origin/main',
+      repo : 'https://github.com/hector7178/client-multiagent.git',
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/nvm/nvm.sh && npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh-options':'ForwardAgent=yes'
+    }
+  }
+};
