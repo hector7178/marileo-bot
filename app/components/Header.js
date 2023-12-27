0b845fbbfd9250c100371c81b6ca66a31531fe20
hector7178 <hjgrimanmarcano@gmail.com>
2023-12-27 11:07:38 -0400

    update

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { useRouter,usePathname } from 'next/navigation'
 import Link from 'next/link'
 function Header() {
@@ -11,14 +11,35 @@ function Header() {
       }
     const router=usePathname()
 
+    const [open, setOpen]=useState(false)
+
+
   return (
     <header className='h-[10vh] w-screen fixed z-10 bg-blue-300 md:bg-transparent items-center flex px-6  md:px-20 py-4 '>
-    <ul className='flex flex-row gap-4 w-full lg:gap-8 justify-end'>
+    <ul className='hidden md:flex flex-row gap-4 w-full lg:gap-8 justify-end '>
+     
       <li><Link href={'/'} className={router===href.inicio?' sm:text-sm md:text-md lg:text-lg hover:opacity-90 text-white border-b-2 border-white':'sm:text-sm  md:text-md lg:text-lg hover:opacity-90 text-white'} >Inicio</Link></li>
       <li><Link href={'/scan'} className={router===href.scan?' sm:text-sm md:text-md lg:text-lg hover:opacity-90 text-white border-b-2 border-white':'sm:text-sm md:text-md lg:text-lg hover:opacity-90 text-white'}>Escanear</Link></li>
       <li><Link href={'/message'} className={router===href.menssage?' sm:text-sm md:text-md lg:text-lg hover:opacity-90 text-white border-b-2 border-white':'sm:text-sm  md:text-md lg:text-lg hover:opacity-90 text-white'}>Mensajes</Link></li>
       <li><Link href={'/configuracion'} className={router===href.configuracion?' sm:text-sm md:text-md lg:text-lg hover:opacity-90 text-white border-b-2 border-white':'sm:text-sm  md:text-md lg:text-lg hover:opacity-90 text-white'}>Configuración</Link></li>
     </ul>
+    {!open
+      ?
+     <svg xmlns="http://www.w3.org/2000/svg" onClick={()=>setOpen(true)} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className='w-8 h-8 absolute top-6 right-6 md:hidden text-white'>
+     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+     </svg>
+     :
+    <ul className='flex md:hidden flex-col bg-blue-300 gap-4 w-1/2 lg:gap-8 absolute top-6 right-0 p-6 items-end justify-end'>
+       <li className="text-white hover:text-red-400 w-8 h-8" onClick={()=>setOpen(false)}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{width:'100%',height:'100%'}} >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+      </li>
+      <li><Link href={'/'} className={router===href.inicio?' sm:text-sm md:text-md lg:text-lg hover:opacity-90 text-white border-b-2 border-white':'sm:text-sm  md:text-md lg:text-lg hover:opacity-90 text-white'} >Inicio</Link></li>
+      <li><Link href={'/scan'} className={router===href.scan?' sm:text-sm md:text-md lg:text-lg hover:opacity-90 text-white border-b-2 border-white':'sm:text-sm md:text-md lg:text-lg hover:opacity-90 text-white'}>Escanear</Link></li>
+      <li><Link href={'/message'} className={router===href.menssage?' sm:text-sm md:text-md lg:text-lg hover:opacity-90 text-white border-b-2 border-white':'sm:text-sm  md:text-md lg:text-lg hover:opacity-90 text-white'}>Mensajes</Link></li>
+      <li><Link href={'/configuracion'} className={router===href.configuracion?' sm:text-sm md:text-md lg:text-lg hover:opacity-90 text-white border-b-2 border-white':'sm:text-sm  md:text-md lg:text-lg hover:opacity-90 text-white'}>Configuración</Link></li>
+    </ul>}
   </header>
   )
 }
--- app/login/page.js
+++ app/login/page.js
@@ -25,28 +25,29 @@ export default function Login(prop) {
     };
 
     if (res?.ok) {
+      route.refresh()
       return route.push('/scan')
     };
     
   }
 
   return (
-    <main className="bg-gradient-to-b from-blue-300 via-blue-600 to-black grid min-h-screen  grid h-screen w-screen p-4 ">
+    <main className="bg-gradient-to-b from-blue-300 via-blue-600 to-black grid min-h-screen items-center justify-center grid h-screen w-full lg:p-4 ">
        
-      <div className="flex flex-col gap-4 lg:w-1/2 w-3/4 h-3/4 bg-white rounded-lg self-center justify-self-center  p-10">
+      <div className="flex flex-col gap-4  w-full h-1/2 lg:h-3/4 bg-white rounded-lg p-6 lg:p-10">
                 
-        <h4 className='text-4xl text-center text-gray-700'>Inicia sesión</h4>
+        <h4 className='text-xl md:text-2xl lg:text-4xl text-center text-gray-700'>Inicia sesión</h4>
         {error && <span className='text-red-500'>*{error }</span>}
-            <form className='h-full bg-gray-100 p-10 flex flex-col gap-6' onSubmit={handleSubmit}>
+            <form className='h-full bg-gray-100 lg:p-10 flex flex-col gap-6' onSubmit={handleSubmit}>
 
               
-              <div className='flex flex-col gap-2'>
-                <label className='text-gray-700 text-lg'>Usuario</label>
-                <input className='text-gray-700 text-lg' placeholder='usuario' name='usuario'></input>
+              <div className='flex flex-col gap-2 p-2 lg:p-0'>
+                <label className='text-gray-700 text-md md:text-lg'>Usuario</label>
+                <input className='text-gray-700 text-md md:text-lg' placeholder='usuario' name='usuario'></input>
               </div>
-            <div className='flex flex-col gap-2'>
-              <label className='text-gray-700 text-lg'>Contraseña</label>
-              <input className='text-gray-700 text-lg' placeholder='...' name='contraseña'></input>
+            <div className='flex flex-col gap-2 p-2 lg:p-0'>
+              <label className='text-gray-700 text-md md:text-lg'>Contraseña</label>
+              <input className='text-gray-700 text-md md:text-lg' placeholder='...' name='contraseña'></input>
             </div>
             <button type='submit' className='rounded-md text-white bg-blue-600 p-2 w-fit self-center'>Iniciar sesión</button>
             </form>
